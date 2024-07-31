41b5a1dd2d77dd98095f6c80b7457c42031d35b4
kakelay <kakelay18052002@gmail.com>
2024-07-31 15:25:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function amk(p1, p2) {
+  return p1 * p2;
+}
+let p1 = 10;
+let result = amk(p1, 3);
+var testing = amk(p1, 21);
+console.log("AMK Data =  " + result);
+console.log("Testing Code = " + testing);
